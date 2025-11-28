98f4cd193d75464b6ccb275088a073358d04c8a7
toshiro <>
2025-11-29 03:26:20 +0900

    雛形作成

---
+++ src/main/java/com/devapp/mst/MstApplication.java
@@ -0,0 +1,13 @@
+package com.devapp.mst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devapp/mst/MstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devapp.mst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
